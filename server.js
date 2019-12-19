b0a74c2336be1128d92ef28f995fee4c994f6331
dantemessy <thepunishar1@gmail.com>
2019-12-19 13:30:56 +0200

    starting Heroku baby

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express') ;
+const server = express() ;
+const PORT = process.env.PORT || 5555 ;
+// process.env.PORT ||
+
+server.get('/test' , (request , response ) => {
+    response.send('working !!!');
+});
+
+server.listen(PORT , () => {
+    console.log('helooo');
+});
+
+
+server.use(express.static('./public'));
